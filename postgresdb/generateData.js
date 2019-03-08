f9b17247926aa5a61326896b4e7bab5921b24aeb
Mina Sorsok <mina.sorsok@gmail.com>
2019-03-08 09:20:51 -0800

    Change permissions on created csv

--- postgresdb/generateData.js
+++ postgresdb/generateData.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const DataStream = require('../postgresdb/dataStream');
 
 module.exports = () => {
-  const file = fs.createWriteStream(`./postgresdb/data.csv`);
+  const file = fs.createWriteStream(`./postgresdb/data.csv`, { mode: 0o755 });
   const data = new DataStream();
   const write = data.pipe(file);
   return new Promise((resolve, reject) => {
