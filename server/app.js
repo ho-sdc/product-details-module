e5ed9ec8a6ddb2b605eb35d13a151fbe93f98b21
Mina Sorsok <mina.sorsok@gmail.com>
2019-03-12 14:46:53 -0700

    make static file serving optional

--- server/app.js
+++ server/app.js
@@ -12,7 +12,9 @@ module.exports.initializeApp = async () => {
   await dbInitialize();
   app.use(parser.json());
   app.use(parser.urlencoded({ extended: true }));
-  app.use(express.static(path.resolve(__dirname, '../client/dist')));
+  if (process.env.SERVE_STATIC) {
+    app.use(express.static(path.resolve(__dirname, '../client/dist')));
+  }
   app.use('/abibas', router);
 };
 
