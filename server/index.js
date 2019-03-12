ba8d232dd91d64b3d6a0a8ba6295ba0aac491099
Mina Sorsok <mina.sorsok@gmail.com>
2019-03-11 19:37:28 -0700

    Add docker to product details

--- server/index.js
+++ server/index.js
@@ -1,8 +1,4 @@
 require('newrelic');
-const dotenv = require('dotenv');
-const dotenvExpand = require('dotenv-expand');
-const myEnv = dotenv.config();
-dotenvExpand(myEnv);
 
 const { app, initializeApp } = require('./app');
 
